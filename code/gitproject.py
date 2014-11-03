eb940b2ff2535848b82d40f81f7eb8683ad87400
wvanamstel <wlvanamstel@gmail.com>
2014-11-03 12:00:52 -0800

    gitproject.py added

---
+++ code/gitproject.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+import pymongo
+
+def connect_to_mongoDB():
+	client = pymongo.MongoClient("localhost", 27017)
+
+if __name__ == '__main__':
+	pass
